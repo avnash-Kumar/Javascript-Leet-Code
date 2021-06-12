cc29d3067f166ac5011f8718c73997c8710f9097
Avinash <avnash.kumar97@gmail.com>
2021-06-13 00:25:31 +0530

    Time: 252 ms (25.93%), Space: 49 MB (37.78%) - LeetHub

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -6,8 +6,6 @@ var isPalindrome = function(x) {
     let numToStr = x.toString();
     let strToArr = numToStr.split('');
     let revNum = '';
-    let maxRange = 2147483648 -1;
-    let minRange = -2147483648;
     for(let i=strToArr.length-1;i>=0;i--){
         if(strToArr[i]=='-'){
             return false;
@@ -16,7 +14,7 @@ var isPalindrome = function(x) {
         }
     }
     let result = revNum;  
-    if(result==numToStr){
+    if(revNum==numToStr){
          return true;
      } else {
          return false;
